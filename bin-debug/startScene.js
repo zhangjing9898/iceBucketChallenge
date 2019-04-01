d0206e2e828dc6ebdd7beb00565f4597ddae0e3f
zhangjing <1105249307@qq.com>
2019-04-01 10:58:49 +0800

    feat: add startScene to main

---
+++ bin-debug/startScene.js
@@ -0,0 +1,33 @@
+var __reflect = (this && this.__reflect) || function (p, c, t) {
+    p.__class__ = c, t ? t.push(c) : t = [c], p.__types__ = p.__types__ ? t.concat(p.__types__) : t;
+};
+var __extends = this && this.__extends || function __extends(t, e) { 
+ function r() { 
+ this.constructor = t;
+}
+for (var i in e) e.hasOwnProperty(i) && (t[i] = e[i]);
+r.prototype = e.prototype, t.prototype = new r();
+};
+var startScene = (function (_super) {
+    __extends(startScene, _super);
+    function startScene() {
+        return _super.call(this) || this;
+    }
+    startScene.prototype.partAdded = function (partName, instance) {
+        _super.prototype.partAdded.call(this, partName, instance);
+    };
+    startScene.prototype.childrenCreated = function () {
+        _super.prototype.childrenCreated.call(this);
+        this.Init();
+    };
+    startScene.prototype.Init = function () {
+        var _this = this;
+        this.height = this.stage.stageHeight;
+        this.btn_Start.addEventListener(egret.TouchEvent.TOUCH_TAP, function () {
+            // this.parent
+            _this.parent.removeChild(_this);
+        }, this);
+    };
+    return startScene;
+}(eui.Component));
+__reflect(startScene.prototype, "startScene", ["eui.UIComponent", "egret.DisplayObject"]);
